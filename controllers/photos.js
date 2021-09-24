30403038e9f6c699223835f1af30bf8623ebabbc
ColinMcCullough <colin.n.mccullough@gmail.com>
2021-09-23 17:27:12 -0700

    refactoring for effeciency

--- controllers/photos.js
+++ controllers/photos.js
@@ -1,24 +1,108 @@
 const axios = require('axios')
-const blockhash = require('blockhash-core')
-const { imageFromBuffer, getImageData } = require('@canvas/image')
+const { imageFromBuffer } = require('@canvas/image')
+const { hash } = require('./imageHashing')
 
-module.exports = {
-  getUniqueImageUrls,
-  compareImages
-}
 /**
- * Deduplicates images and returns array of objects
- * containing image url and size
+ * Deduplicates array of image urls by file name and image 
+ * render returning lategest images when duplicates are found
  * @param {Array} images - array of image urls
- * @returns {Array} - returns an array of objects 
+ * @returns {Array} - returns an array of url 
  */
-async function getUniqueImageUrls(images) {
-  const refMap = await _generateRefMap(images)
+const getUniqueImageUrls = async (imageUrls) => {
+  const uniqueImageMap = await _dedupeByFileName(imageUrls)
+  const imgMap = await _dedupeByImageRender(uniqueImageMap)
   const uniqueImages = []
-  for (let [key, value] of refMap) {
+  for (let [key, value] of imgMap) {
     uniqueImages.push(value.url)
   }
   return uniqueImages
+}
+
+/**
+ * De deplicates array of image urls by file name
+ * returning largest version of image when duplicates are found
+ * @param {Array<String>} images
+ * @returns {Map<String,Object>} - map of image names to image objects
+ */
+const _dedupeByFileName = async (images) => {
+  const imageMap = new Map()
+  const imageObjects = await _getImageObjects(images)
+  for(let i = 0; i < imageObjects.length; i++) {
+    const imgObj = imageObjects[i]
+    if (imgObj) {
+      const imgKey = imgObj.url.split('/').pop()
+      const imageVal = getMapVal(imageMap, imgKey, imgObj)
+      imageMap.set(imgKey, imageVal)
+    }
+  }
+  return imageMap
+}
+
+/**
+ * Fetches image objects from array of image urls
+ * @param {Array<String>} images
+ * @returns {Array<Object>} - array of image objects
+ */
+const _getImageObjects = async (images) => {
+  return Promise.all(images.map(image => _readImageFromUrl(image)))
+}
+
+/**
+ * Creates map of hashes to images arrays. Images resulting in the same hash will
+ * be replaced if they are larger than whats hashed under the same hash
+ * @param {Map<String,Object>} images - map of images object where key is image name and value is image object
+ * @returns {Map<String,Object>} - map of image urls to hashes e.g. { hash: { url, height, width}} }
+ */
+const _dedupeByImageRender = async (images) => {
+  const refMap = new Map();
+  const imageIterator = images.values()
+  let currentImage = imageIterator.next()
+  while (currentImage.value) {
+    const image = currentImage.value
+    const imgHash = await hash(image)
+    const imageVal = getMapVal(refMap, imgHash, image)
+    refMap.set(imgHash, imageVal)
+    currentImage = imageIterator.next()
+  }
+  return refMap
+}
+
+/**
+ * Looks to see if an image key is in map, if it is and
+ * image stored in map is larger than the image passed in
+ * then it returns the image strored, otherwise returns new image
+ * @param {Map} imageMap
+ * @param {String} imgKey
+ * @param {Object} newImage - image object with width property
+ * @returns {Object} - looks if there is a
+ */
+ const getMapVal = (imageMap, imgKey, newImage) => {
+  let value = newImage
+  if (imageMap.has(imgKey)) {
+    const currentImg = imageMap.get(imgKey)
+    if (currentImg.width > newImage.width) {
+      value = currentImg
+    }
+  }
+  return value
+}
+
+/**
+ * Fetches image from url and returns image buffer
+ * @param {String} url
+ * @returns {Object} - image object from @canvas/image imageFromBuffer fnc
+ */
+const _readImageFromUrl = async (url) => {
+  try {
+    const response = await axios.get(url, {
+      responseType: "arraybuffer"
+    })
+    const img = await imageFromBuffer(response.data)
+    img.url = url
+    return img
+  } catch (error) {
+    return null
+  }
 }
 
 /**
@@ -27,111 +111,13 @@ async function getUniqueImageUrls(images) {
  * @param {String} imgUrl2
  * @returns {Number} - similarity percentage
  */
-async function compareImages(imgUrl1, imgUrl2) {
+const compareImages = async (imgUrl1, imgUrl2) => {
   const img1 = await _readImageFromUrl(imgUrl1)
   const img2 = await _readImageFromUrl(imgUrl2)
-  const hash1 = await _hash(img1)
-  const hash2 = await _hash(img2)
+  const hash1 = await hash(img1)
+  const hash2 = await hash(img2)
   return _calculateSimilarity(hash1, hash2)
 }
-
-/**
- * Creates map of hashes to images arrays.
- * Images resulting in the same hash will be replcaed if they are larger than whats hashed
- * @param {Array} images - array of image urls
- * @returns {Map} - map of image urls to hashes e.g. { hash: { url, height, width}} }
- */
-async function _generateRefMap(images) {
-  const refMap = new Map();
-  for (let i = 0; i < images.length; i++) {
-    const imgObj = await _readImageFromUrl(images[i])
-    if (imgObj) {
-      const { height, width } = imgObj
-      const imgHash = await _hash(imgObj)
-      let value
-      if (refMap.has(imgHash)) {
-        const existingPath = refMap.get(imgHash)
-        value = existingPath.height > height ? existingPath : { url: images[i], height, width }
-      } else {
-        value= { url: images[i], height, width }
-      }
-      refMap.set(imgHash, value)
-    }
-  }
-  return refMap
-}
-
-
-/**
- * Hashes image url into binary string
- * @param {String} imageUrl
- * @returns {String} - binary string of image hash
- */
-async function _hash(imageObj) {
-  try {
-    const hash = await blockhash.bmvbhash(getImageData(imageObj), 8)
-    return _hexToBin(hash)
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-
-/**
- * Fetches image from url and returns image buffer
- * @param {String} url
- * @returns {Buffer} - image buffer
- */
-async function _readImageFromUrl(url) {
-  try {
-    const response = await axios.get(url, {
-      responseType: "arraybuffer"
-    })
-    return imageFromBuffer(response.data)
-  } catch (error) {
-    console.log(error)
-    return null
-  }
-}
-
-
-/**
- * Builds a binary string from a hex string
- * @param {String} hexString
- * @returns {String} Binary String
- */
-function _hexToBin(hexString) {
-  const hexBinLookup = {
-    0: "0000",
-    1: "0001",
-    2: "0010",
-    3: "0011",
-    4: "0100",
-    5: "0101",
-    6: "0110",
-    7: "0111",
-    8: "1000",
-    9: "1001",
-    a: "1010",
-    b: "1011",
-    c: "1100",
-    d: "1101",
-    e: "1110",
-    f: "1111",
-    A: "1010",
-    B: "1011",
-    C: "1100",
-    D: "1101",
-    E: "1110",
-    F: "1111",
-  };
-  let result = "";
-  for (i = 0; i < hexString.length; i++) {
-    result += hexBinLookup[hexString[i]]
-  }
-  return result
-}
-
 
 /**
  * Calculates similarity percentage between two hashes
@@ -139,11 +125,16 @@ function _hexToBin(hexString) {
  * @param {String} hash2
  * @returns {Number} - similarity percentage
  */
-function _calculateSimilarity(hash1, hash2) {
-  let similarity = 0;
+const _calculateSimilarity = (hash1, hash2) => {
+  let similarity = 0
   hash1Array = hash1.split("");
   hash1Array.forEach((bit, index) => {
-    hash2[index] === bit ? similarity++ : null;
-  });
-  return parseInt((similarity / hash1.length) * 100);
+    hash2[index] === bit ? similarity++ : null
+  })
+  return parseInt((similarity / hash1.length) * 100)
+}
+
+module.exports = {
+  getUniqueImageUrls,
+  compareImages
 }

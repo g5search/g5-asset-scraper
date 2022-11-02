605efb4da318c23c362c618b3d2313237d0813cb
David Miller <15317284+pcktbot@users.noreply.github.com>
2022-11-02 11:11:54 -0500

    timer and redis

--- controllers/queue.js
+++ controllers/queue.js
@@ -8,20 +8,19 @@ const Bee = require('bee-queue')
 module.exports = function (app) {
   const queue = new Bee('scraper', {
     isWorker: true,
-    redis: {
-      host: process.env.REDIS_HOST,
-      port: process.env.REDIS_PORT
-    }
+    redis: { url: process.env.REDIS_URL }
   })
   queue.process(async (job, done) => {
+    console.time('SCRAPE_JOB')
     const { data } = job
     const Scraper = require('../scraper')
     try {
       const scraper = new Scraper(data)
       await scraper.run()
       const results = scraper.results()
-      // if (process.env.ENABLE_LOGGING) console.log(JSON.stringify(results))
+      if (process.env.ENABLE_LOGGING) console.log(JSON.stringify(results))
       // pubsub publish message with results
+      console.timeEnd('SCRAPE_JOB')
       done(null, results)
     } catch (error) {
       done(error)

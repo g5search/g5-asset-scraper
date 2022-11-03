c757acb303902ffd38174e67bf72083cc4fbb8c2
David Miller <15317284+pcktbot@users.noreply.github.com>
2022-11-03 16:08:27 -0500

    working

--- controllers/pubsub.js
+++ controllers/pubsub.js
@@ -1,5 +1,7 @@
 const { PubSub } = require('@google-cloud/pubsub')
-const pubsub = new PubSub()
+// const queue = require('./queue')()
+const projectId = process.env.PROJECT_ID
+const pubsub = new PubSub({ projectId })
 const subscriptionNameOrId = process.env.PUBSUB_SUBSCRIPTION
 const maxMessages = 3
 const timeout = 10
@@ -9,19 +11,27 @@ const timeout = 10
  * @param {Object} queue Bee worker queue instance
  * @returns 
  */
-module.exports = function (queue) {
+module.exports = function () {
   return {
-    async subscribeWithFlowControl () {
+    subscribeWithFlowControl (queue) {
       const subscriberOptions = {
         flowControl: { maxMessages }
       }
       const subscription = pubsub.subscription(subscriptionNameOrId, subscriberOptions)
-      console.log(`******* Listening for messages on ${subscription.name}.`)
+      console.log(`******* Listening for messages on ${subscription.name}`)
       const messageHandler = (message) => {
         console.log(`******* Received message ${message.id}:`)
         console.log(`******* Data: ${message.data}`)
         console.log(`******* Attributes: ${message.attributes}`)
-        queue.createJob(message.data).save()
+        console.log(`******* Queue is Ready: ${queue._isReady}`)
+        try {
+          let data
+          data = Buffer.from(message.data, 'base64').toString().trim()
+          data = JSON.parse(data)
+          queue.createJob(data).save()
+        } catch (error) {
+          console.error(`******* Error: ${error}`)
+        }
         message.ack()
       }
       subscription.on('message', messageHandler)
@@ -29,6 +39,15 @@ module.exports = function (queue) {
         subscription.close()
       }, timeout * 1000)
     },
-    publish (message) {}
+    async publish (message) {
+      const dataBuffer = Buffer.from(JSON.stringify(message))
+      
+      try {
+        const messageId = await pubsub.topic(process.env.PUBSUB_TOPIC).publishMessage(dataBuffer)
+        console.log(`******* Message ${messageId} published.`)
+      } catch (error) {
+        
+      }
+    }
   }
 }
